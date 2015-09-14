a3b6ac35432511f33b83bcdfa442e2e7dbe4a758
Vyshak N <vyshak.n.93@gmail.com>
2015-09-14 18:00:15 +0530

    Ex 5 done

--- program_04.js
+++ program_04.js
@@ -1,12 +1,13 @@
 var fs = require('fs');
 var list = [];
-var extList =[];
+var extList = [];
 fs.readdir(process.argv[process.argv.length - 2], function (err, list) {
      for (var i = 0; i < list.length; i++) {
         //console.log(string.split('\n').length -1);
         var temp = list[i].split('.');
         //console.log(temp);
-        if ( temp.length > 1 && temp[temp.length - 1] === process.argv[process.argv.length - 1]) {
+        if ( temp.length > 1 && temp[temp.length - 1] ===
+                 process.argv[process.argv.length - 1]) {
             console.log(list[i]);
         }
     }
--- program_05a.js
+++ program_05a.js
@@ -1,14 +1,10 @@
-var fs = require('fs');
-var list = [];
-var extList =[];
-fs.readdir(process.argv[process.argv.length - 2], function (err, list) {
-     for (var i = 0; i < list.length; i++) {
-        //console.log(string.split('\n').length -1);
-        var temp = list[i].split('.');
-        //console.log(temp);
-        if ( temp.length > 1 && temp[temp.length - 1] === process.argv[process.argv.length - 1]) {
-            console.log(list[i]);
+var modl = require('./program_05b.js');
+
+
+modl(process.argv[process.argv.length - 2],
+        process.argv[process.argv.length - 1],
+        function(err, data) {
+                for (var i = 0; i < data.length; i++) {
+                        console.log(data[i]);
         }
-    }
-    //console.log(list);
 });
